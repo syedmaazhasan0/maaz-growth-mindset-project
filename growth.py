97304c38ab4eea0af72f963cc3f181a5afe69922
syedmaazhasan0 <160649717+syedmaazhasan0@users.noreply.github.com>
2025-03-11 13:29:10 +0500

    growth.py

--- growth.py
+++ growth.py
@@ -2,10 +2,7 @@ import streamlit as st # type: ignore
 import pandas as pd # type: ignore
 import os
 from io import BytesIO
-import openpyxl as openpyxl # type: ignore
-pd
-openpyxl
-streamlit # type: ignore
+
 
 st.set_page_config(page_title = "Data Sweeper", layout = 'wide')
 
